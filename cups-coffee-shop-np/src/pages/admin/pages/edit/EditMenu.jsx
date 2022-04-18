a65bf7e48eb7a95a896d0d1b12a35907d1f4a7f2
i7p7x0 <i7p7x0@gmail.com>
2022-04-18 11:08:06 +0545

    added conditional rendering in edit menu

--- cups-coffee-shop-np/src/pages/admin/pages/edit/EditMenu.jsx
+++ cups-coffee-shop-np/src/pages/admin/pages/edit/EditMenu.jsx
@@ -3,9 +3,9 @@ import EditForm from "../../../../components/editForm/EditForm";
 import { useSelector } from "react-redux";
 import routes from "../../../../constants/routes";
 import { useNavigate } from "react-router-dom";
-import { Table, Button, Form, InputGroup, FormControl } from "react-bootstrap";
+import { Button, Form, InputGroup, FormControl } from "react-bootstrap";
 import menu from "../../../../data/menu";
-import { FaEdit } from "react-icons/fa";
+
 import { MdDelete } from "react-icons/md";
 import { Link } from "react-router-dom";
 import "./edit-menu.css";
@@ -32,33 +32,12 @@ const EditMenu = (props) => {
     deleteFlag: false,
   });
 
-  // method to clear updateItem
-  const clearUpdateItem = () => {
-    setUpdateItem({
-      parentId: "",
-      itemId: "",
-      itemCategory: "",
-      itemName: "",
-      itemPrice: "",
-      deleteFlag: false,
-    });
-  };
-
   // tracks state of item that is being added
   const [newItem, setNewItem] = useState({
     itemCategory: "",
     itemName: "",
     itemPrice: "",
   });
-
-  // method to clear new item
-  const clearNewItem = () => {
-    setNewItem({
-      itemCategory: "",
-      itemName: "",
-      itemPrice: "",
-    });
-  };
 
   // handles updates and delete changes
   const handleUpdateChanges = (event) => {
@@ -111,12 +90,7 @@ const EditMenu = (props) => {
     }
   };
 
-  const handleSubmitMenu = () => {
-    console.log(updateItem);
-    console.log(newItem);
-  };
-
-  // handle state changes for inputs on add new item
+  // handle add changes
   const handleAddItem = (event) => {
     event.preventDefault();
     let changeType = event.target.id;
@@ -155,110 +129,159 @@ const EditMenu = (props) => {
     }
   };
 
+  // method to clear updateItem
+  const clearUpdateItem = () => {
+    setUpdateItem({
+      parentId: "",
+      itemId: "",
+      itemCategory: "",
+      itemName: "",
+      itemPrice: "",
+      deleteFlag: false,
+    });
+  };
+  // method to clear new item
+  const clearNewItem = () => {
+    setNewItem({
+      itemCategory: "",
+      itemName: "",
+      itemPrice: "",
+    });
+  };
+
+  // handle clear press
+  const handleClearButtonPress = (event) => {
+    event.preventDefault();
+    clearUpdateItem();
+    clearNewItem();
+  };
+
+  // handle submit button press
+  const handleSubmitMenu = () => {
+    console.log(updateItem);
+    console.log(newItem);
+  };
+
   return (
     <div className="edit-menu">
       <EditForm header={"Edit your shop menu"}>
         <Form>
+          {(updateItem.itemName.length === 0 &&
+            newItem.itemName.length === 0) ||
+          (updateItem.itemName.length > 0 && newItem.itemName.length === 0) ? (
+            <>
+              <Form.Select
+                aria-label="Edit a menu item"
+                defaultValue="default"
+                className="edit-info-select-form mb-3"
+                id="edit-menu-update-select"
+                onChange={handleUpdateChanges}
+              >
+                <option value="default" disabled>
+                  --Select a menu item to edit--
+                </option>
+                {menu.map((m) => {
+                  return m.items.map((item) => {
+                    return (
+                      <option
+                        key={item._id}
+                        value={m._id + "childId" + item._id}
+                      >
+                        {item.item}
+                      </option>
+                    );
+                  });
+                })}
+              </Form.Select>
+              <InputGroup size="sm" className="mb-3">
+                <InputGroup.Text
+                  id="inputGroup-sizing-sm"
+                  className="edit-info-input-group"
+                >
+                  Item
+                </InputGroup.Text>
+                <FormControl
+                  aria-label="opening time hh"
+                  placeholder="Item"
+                  id="edit-menu-update-item"
+                  value={updateItem.itemName}
+                  disabled
+                />
+                <FormControl
+                  aria-label="closing time mm"
+                  placeholder="Price"
+                  id="edit-menu-update-price"
+                  onChange={handleUpdateChanges}
+                  value={updateItem.itemPrice}
+                />
+                {/* Delete existing item */}
+                <Button
+                  variant="danger"
+                  id="edit-menu-delete-item"
+                  onClick={handleUpdateChanges}
+                >
+                  <MdDelete id="edit-menu-delete-item-icon" />
+                </Button>
+              </InputGroup>
+            </>
+          ) : null}
           {/* update existing item's price */}
-          <Form.Select
-            aria-label="Edit a menu item"
-            defaultValue="default"
-            className="edit-info-select-form mb-3"
-            id="edit-menu-update-select"
-            onChange={handleUpdateChanges}
-          >
-            <option value="default" disabled>
-              --Select a menu item to edit--
-            </option>
-            {menu.map((m) => {
-              return m.items.map((item) => {
-                return (
-                  <option key={item._id} value={m._id + "childId" + item._id}>
-                    {item.item}
+          {(updateItem.itemName.length === 0 &&
+            newItem.itemName.length === 0) ||
+          (updateItem.itemName.length === 0 && newItem.itemName.length > 0) ? (
+            <>
+              <span>or</span>
+              <br />
+              <br />
+              {/* Add new item */}
+              <InputGroup className="mb-3 edit-info-input-group">
+                <InputGroup.Text className="edit-info-input-group">
+                  Add a new item
+                </InputGroup.Text>
+                <Form.Select
+                  aria-label="select-timings"
+                  defaultValue={"default"}
+                  className="edit-info-select-form"
+                  id="edit-add-item-select"
+                  onChange={handleAddItem}
+                >
+                  <option value="default" disabled>
+                    --Category--
                   </option>
-                );
-              });
-            })}
-          </Form.Select>
-          <InputGroup size="sm" className="mb-3">
-            <InputGroup.Text
-              id="inputGroup-sizing-sm"
-              className="edit-info-input-group"
-            >
-              Item
-            </InputGroup.Text>
-            <FormControl
-              aria-label="opening time hh"
-              placeholder="Item"
-              id="edit-menu-update-item"
-              value={updateItem.itemName}
-              disabled
-            />
-            <FormControl
-              aria-label="closing time mm"
-              placeholder="Price"
-              id="edit-menu-update-price"
-              onChange={handleUpdateChanges}
-              value={updateItem.itemPrice}
-            />
-            {/* Delete existing item */}
-            <Button
-              variant="danger"
-              id="edit-menu-delete-item"
-              onClick={handleUpdateChanges}
-            >
-              <MdDelete id="edit-menu-delete-item-icon" />
-            </Button>
-          </InputGroup>
-          <span>or</span>
-          <br />
-          <br />
-          {/* Add new item */}
-          <InputGroup className="mb-3 edit-info-input-group">
-            <InputGroup.Text className="edit-info-input-group">
-              Add a new item
-            </InputGroup.Text>
-            <Form.Select
-              aria-label="select-timings"
-              defaultValue={"default"}
-              className="edit-info-select-form"
-              id="edit-add-item-select"
-              onChange={handleAddItem}
-            >
-              <option value="default" disabled>
-                --Category--
-              </option>
-              {menu.map((m) => {
-                return (
-                  <option key={m._id} value={m.category}>
-                    {m.category}
-                  </option>
-                );
-              })}
-            </Form.Select>
-            <br />
-          </InputGroup>
-          <InputGroup className="mb-3 edit-info-input-group">
-            <FormControl
-              aria-label="opening time hh"
-              placeholder="Item"
-              id="edit-add-item"
-              value={newItem.itemName}
-              onChange={handleAddItem}
-            />
-            <FormControl
-              aria-label="closing time mm"
-              placeholder="Price"
-              id="edit-add-item-price"
-              value={newItem.itemPrice}
-              onChange={handleAddItem}
-            />
-          </InputGroup>
-
+                  {menu.map((m) => {
+                    return (
+                      <option key={m._id} value={m.category}>
+                        {m.category}
+                      </option>
+                    );
+                  })}
+                </Form.Select>
+                <br />
+              </InputGroup>
+              <InputGroup className="mb-3 edit-info-input-group">
+                <FormControl
+                  aria-label="opening time hh"
+                  placeholder="Item"
+                  id="edit-add-item"
+                  value={newItem.itemName}
+                  onChange={handleAddItem}
+                />
+                <FormControl
+                  aria-label="closing time mm"
+                  placeholder="Price"
+                  id="edit-add-item-price"
+                  value={newItem.itemPrice}
+                  onChange={handleAddItem}
+                />
+              </InputGroup>
+            </>
+          ) : null}
           <Button variant="success mx-3" onClick={handleSubmitMenu}>
             Submit
           </Button>
-          <Button variant="danger">Clear</Button>
+          <Button variant="danger" onClick={handleClearButtonPress}>
+            Clear
+          </Button>
           <Link to={routes.adminDashboard}>
             <Button variant="secondary mx-3">Go Back</Button>
           </Link>
