d67ec017ddf860a0a6e1eb958a3cd7be900290f1
i7p7x0 <i7p7x0@gmail.com>
2022-04-23 12:41:28 +0545

    Designed about page

--- app/src/components/dataList/DataList.jsx
+++
@@ -1,7 +0,0 @@
-import { React } from "react";
-
-const DataList = (props) => {
-  return <div className>DataList</div>;
-};
-
-export default DataList;
--- app/src/pages/about/About.jsx
+++ app/src/pages/about/About.jsx
@@ -1,6 +1,5 @@
 import { React, useState } from "react";
 import Card from "../../components/card/Card";
-import DataList from "../../components/dataList/DataList";
 import LoadingSpinner from "../../components/loadingSpinner/LoadingSpinner";
 import useGetAPICall from "../../hooks/useGetAPICall";
 import "./about.css";
@@ -8,6 +7,17 @@ import inBench from "../../images/inBench.jpg";
 import wheelie from "../../images/wheelie.jpg";
 import outTree from "../../images/outTree.jpg";
 import ImageContainer from "../../components/imageContainer/ImageContainer";
+import {
+  MdStore,
+  MdOutlineMenu,
+  MdInfo,
+  MdEmail,
+  MdPhoneInTalk,
+  MdLocationPin,
+  MdOutlineAccessTimeFilled,
+} from "react-icons/md";
+import { RiInstagramFill } from "react-icons/ri";
+import { BsPatchQuestionFill, BsDash } from "react-icons/bs";
 
 const About = (props) => {
   const [contactDetails, setContactDetails] = useState([]);
@@ -23,7 +33,9 @@ const About = (props) => {
             <div className="about-container-child about-b">
               <Card>
                 <div>
-                  <h1 className="about-child-header">About us</h1>
+                  <h3 className="about-child-header">
+                    About us <BsPatchQuestionFill />
+                  </h3>
                   <hr />
                   <p className="about-child-paragraph">
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit.
@@ -41,7 +53,9 @@ const About = (props) => {
             <div className="about-container-child about-a">
               <Card>
                 <div className="about-inner-child">
-                  <h1 className="about-child-header">Our Shop</h1>
+                  <h3 className="about-child-header">
+                    Our Shop <MdStore />
+                  </h3>
                   <hr />
                   <div className="child-body-segment">
                     <div className="child-image">
@@ -68,7 +82,9 @@ const About = (props) => {
             <div className="about-container-child about-c">
               <Card>
                 <div>
-                  <h1 className="about-child-header">Our Menu </h1>
+                  <h3 className="about-child-header">
+                    Our Menu <MdOutlineMenu />
+                  </h3>
                   <hr />
                   <div className="child-body-segment">
                     <div className="child-image">
@@ -94,7 +110,9 @@ const About = (props) => {
             <div className="about-container-child about-d">
               <Card>
                 <div>
-                  <h1 className="about-child-header">Our Detail </h1>
+                  <h3 className="about-child-header">
+                    Our Detail <MdInfo />
+                  </h3>
                   <hr />
                   <div className="child-body-segment">
                     <div className="child-image">
@@ -106,21 +124,34 @@ const About = (props) => {
                     <div className="child-body">
                       <p className="about-child-paragraph">
                         Lorem, ipsum dolor sit amet consectetur adipisicing
-                        elit. Necessitatibus quibusdam odit quasi voluptates
-                        dolor facere consectetur. Incidunt eos deserunt ratione,
-                        asperiores ex neque ipsum ab, eveniet, dolorem iste
-                        quasi rem?
+                        elit.
                       </p>
                       <div className="about-contact-details-container">
                         <ul>
-                          <li>{contactDetails[0].emailAddress}</li>
-                          <li>{contactDetails[0].contactNumber}</li>
-                          <li>{contactDetails[0].location}</li>
-                          <li>{contactDetails[0].instagramHandle}</li>
+                          <li className="contact-details-list-item">
+                            <MdEmail /> {contactDetails[0].emailAddress}
+                          </li>
+                          <li className="contact-details-list-item">
+                            <MdPhoneInTalk /> {contactDetails[0].contactNumber}
+                          </li>
+                          <li className="contact-details-list-item">
+                            <MdLocationPin /> {contactDetails[0].location}
+                          </li>
+                          <li className="contact-details-list-item">
+                            <RiInstagramFill />{" "}
+                            {contactDetails[0].instagramHandle}
+                          </li>
+                          <span>
+                            <MdOutlineAccessTimeFilled /> Timings
+                          </span>
                           {contactDetails[0].timings.map((timing) => {
                             return (
-                              <li key={timing._id}>
-                                {timing.day} {timing.openingTime}
+                              <li
+                                key={timing._id}
+                                className="contact-details-list-item"
+                              >
+                                {timing.day} : 
+                                {timing.openingTime} <BsDash />
                                 {timing.closingTime}
                               </li>
                             );
